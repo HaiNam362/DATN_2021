fd563146848b5df7c0a637e5f0871bd7280d3110
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-12-04 22:42:49 +0700

    Fix message

--- src/auth/auth.Controller.js
+++ src/auth/auth.Controller.js
@@ -7,7 +7,7 @@ export const register = async (req, res, next) => {
     try {
         const user = await User.create(req.body);
         res.status(200).json({
-            status: 'register success',
+            status: 'Register success',
             result: user
         })
     } catch (error) {
@@ -23,14 +23,14 @@ export const login = async (req, res, next) => {
         if (!user) {
             res.status(400).json({
                 status: 'not found',
-                message: 'email is not correct',
+                message: 'Email is not correct',
             })
         } else {
             let check = await bcrypt.compareSync(req.body.passWord, user.password);
             if (user.email === email && check == true) {
                 const token = jwt.sign({ userId: user._id }, 'project', { algorithm: 'HS256' });
                 res.status(200).json({
-                    status: 'login success',
+                    status: 'Login success',
                     data: {
                         user: user,
                         token, userName: user.name,
@@ -39,7 +39,7 @@ export const login = async (req, res, next) => {
             } else {
                 res.status(400).json({
                     status: false,
-                    message: 'password do not exist',
+                    message: 'Password do not exist',
                 })
             }
         }
@@ -51,7 +51,7 @@ export const getListUser = async (req, res, next) => {
     try {
         const user = await User.find({});
         return res.status(200).json({
-            status: 'get list user success',
+            status: 'Get list user success',
             result: user.length,
             data: { user }
         })
@@ -64,7 +64,7 @@ export const getListUserId = async (req, res, next) => {
     try {
         const user = await User.findOne({ _id: id });
         res.status(200).json({
-            status: 'get list user + id success',
+            status: 'Get list user + id success',
             data: { user }
         })
     } catch (error) {
@@ -75,7 +75,7 @@ export const UpdateOneUser = async (req, res, next) => {
     try {
         if (!req.body) {
             return res.status(400).send({
-                message: 'data to update can not be empty!'
+                message: 'Data to update can not be empty!'
             })
         }
         const id = req.params.id;
@@ -86,7 +86,7 @@ export const UpdateOneUser = async (req, res, next) => {
         const userDB2 = await User.findByIdAndUpdate(id, { $set: req.body }, { new: true });
         await userDB2.save();
         return res.status(200).send({
-            message: 'update user successfully',
+            message: 'Update user successfully',
             data: userDB2
         })
     } catch (error) {
@@ -99,7 +99,7 @@ export const DelateOneUser = async (req, res, next) => {
         if (!user) {
             res.status(404).send(" No item");
         }
-        res.status(200).send("delete user successfully");
+        res.status(200).send("Delete user successfully");
     } catch (error) {
         res.status(500).send(error);
     }
@@ -114,7 +114,7 @@ export const uploadAvatar = async (req, res, next) => {
         }
         console.log(req.file.filename, "9876");
         res.status(200).send({
-            message: 'upload avatar successfully',
+            message: 'Apload avatar successfully',
             data: userDB
         })
     } catch (error) {
@@ -126,7 +126,7 @@ export const createEmployeeAccount = async (req, res, next) => {
     try {
         const user = await User.create(req.body);
         res.status(200).json({
-            status: 'register success',
+            status: 'Register success',
             result: user
         })
     } catch (error) {
@@ -138,7 +138,7 @@ export const getEmployees = async (req, res, next) => {
     try {
         const user = await User.find({ role: "employee" })
         return res.status(200).json({
-            message: "get employee",
+            message: "Get employee",
             result: user.length,
             data: { user }
         })
@@ -178,7 +178,7 @@ export const changePassword = async (req, res, next) => {
             })
         } else {
             return res.status(400).send({
-                message: 'password does not matched',
+                message: 'Password does not matched',
                 data: {}
             })
         }
