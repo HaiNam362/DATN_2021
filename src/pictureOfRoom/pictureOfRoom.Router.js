87b8951b1a7e427da72fde5cb4a01b53573463ce
namnhph11371 <namnhph11371@fpt.edu.vn>
2021-12-12 00:55:01 +0700

    update14

--- src/pictureOfRoom/pictureOfRoom.Router.js
+++ src/pictureOfRoom/pictureOfRoom.Router.js
@@ -10,6 +10,6 @@ Router.put('/createPictureOfRoom', upload.single('picture'), pictureOfRoomContro
 Router.post('/uploadPicture', uploadPicture, pictureOfRoomController.createPictureOfRoom);
 Router.put('/updatePictureOfRoom',uploadPicture, pictureOfRoomController.updatePictureOfRoom);
 Router.get('/getPictureOfRoom',pictureOfRoomController.getPictureAndPrice);
-Router.get('/getPrice',pictureOfRoomController.getPrice);
+Router.get('/getPrice/:price',pictureOfRoomController.getPrice);
 
 export { Router }
